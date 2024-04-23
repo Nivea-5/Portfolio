431b39cd8d1d743bb86210e52dae857097c805b0
Maël GARNIER <m.garnier@alpi.link>
2024-04-23 11:23:58 +0200

    mise en place d'un JSON pour le contenu de la page de compétences

--- js/competences.js
+++ js/competences.js
@@ -2,10 +2,54 @@ import Utils from "./utils.js"
 
 const utils = new Utils();
 
+let appearsOnScrollElements = document.querySelectorAll(".appearsOnScroll");
+
 utils.drawNavBar("competences");
 utils.drawFooter();
 
-const appearsOnScrollElements = document.querySelectorAll(".appearsOnScroll");
+function drawSection(content) {
+    let section = document.createElement("section");
+
+    let img = document.createElement("img");
+    img.src = "../img/" + content.img;
+    img.classList.add("appearsOnScroll");
+
+    let mainDiv = document.createElement("div");
+    
+    let title = document.createElement("h2");
+    title.innerText = content.title;
+
+    let tags = document.createElement("div");
+    tags.classList.add("tags");
+    content.tags.forEach(tag => {
+        let span = document.createElement("span");
+        span.innerText = tag;
+        tags.appendChild(span);
+    });
+
+    let desc = document.createElement("p");
+    desc.innerText = content.description;
+
+    mainDiv.appendChild(title);
+    mainDiv.appendChild(tags)
+    mainDiv.appendChild(desc);
+
+    section.appendChild(img);
+    section.appendChild(mainDiv);
+
+    document.querySelector("main").appendChild(section);
+}
+
+utils.getContent("competences").then(data => {
+    data = JSON.parse(data);
+
+    data.forEach(section => {
+        drawSection(section);
+    });
+
+    appearsOnScrollElements = document.querySelectorAll(".appearsOnScroll");
+})
+
 
 document.querySelector(".container").addEventListener("scroll", () => {
     appearsOnScrollElements.forEach((element) => {
